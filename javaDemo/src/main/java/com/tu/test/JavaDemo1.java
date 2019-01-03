dd879cd6238e1d4faca18f03dfa7acee362802b1
tuyongjian <1475591137@qq.com>
2019-01-03 22:28:55 +0800

    java study

--- javaDemo/src/main/java/com/tu/test/JavaDemo1.java
+++ javaDemo/src/main/java/com/tu/test/JavaDemo1.java
@@ -4,11 +4,8 @@ package com.tu.test;
  * Created by tuyongjian on 2018/9/15.
  */
 public class JavaDemo1 {
-
-
-    public static void main(String[] args) {
-        Integer f1=100,f2=100,f3=150,f4=150;
-        System.out.println(f1==f2);
-        System.out.println(f3==f4);
-    }
+        static String s;
+        public static void main (String[]args) {
+            System.out.println ("s=" + s);
+        }
 }
