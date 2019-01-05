85d91adf9125836a4f8922f32e1566c30eff0961
tuyongjian <1475591137@qq.com>
2019-01-05 17:07:55 +0800

    java study

---
+++ common/src/main/java/com/tu/common/dto/Result.java
@@ -0,0 +1,58 @@
+package com.tu.common.dto;
+
+/**
+ * Created by tuyongjian on 2019/1/5.
+ * 通用接口请求接口DTO
+ */
+public class Result {
+
+    /**
+     * 是否成功的标志
+     */
+    private boolean success;
+
+    /**
+     * 返回的信息
+     */
+    private String  message;
+
+    /**
+     * 返回的结果
+     */
+    private String result;
+
+    public Result(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Result(boolean success, String message, String result) {
+        this.success = success;
+        this.message = message;
+        this.result = result;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
--- javaDemo/src/main/java/com/tu/test/JavaDemo1.java
+++ javaDemo/src/main/java/com/tu/test/JavaDemo1.java
@@ -6,6 +6,23 @@ package com.tu.test;
 public class JavaDemo1 {
         static String s;
         public static void main (String[]args) {
-            System.out.println ("s=" + s);
+
+          int total =0;
+            for (int i = 0; i <4 ; i++) {
+                if(i==1){
+                    continue;
+                }
+                if (i==2){
+                    break;
+                }
+
+                total+=i;
+            }
+            System.out.println(total);
+
+            String str1= "hello";
+            String str2= "he"+new String("llo");
+            System.out.println(str1==str2);
+            System.out.println(str1.equals(str2));
         }
 }
