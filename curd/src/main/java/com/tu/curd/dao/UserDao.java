d41787d4da249b5cdbe157c3144b839a0488fcb7
tuyongjian <1475591137@qq.com>
2019-01-06 22:16:24 +0800

    java study

---
+++ curd/src/main/java/com/tu/curd/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.tu.curd.dao;
+
+import com.tu.curd.model.User;
+
+/**
+ * Created by tuyongjian on 2019/1/6.
+ */
+public interface UserDao {
+
+    public User selectUser(int id);
+}
---
+++ curd/src/main/java/com/tu/curd/service/IUserService.java
@@ -0,0 +1,11 @@
+package com.tu.curd.service;
+
+import com.tu.curd.model.User;
+
+/**
+ * Created by tuyongjian on 2019/1/6.
+ */
+public interface IUserService {
+
+    public User queryUser(int id);
+}
