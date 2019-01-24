fceab96a6b20d2363062d7ce8acb556c4eb7c024
tuyongjian <1475591137@qq.com>
2019-01-24 14:22:40 +0800

    spring 学习。。。。。。。。。。。。

---
+++ service/src/main/java/com/tu/service/serviceImpl/MyRealm.java
@@ -0,0 +1,22 @@
+package com.tu.service.serviceImpl;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by len on 2019/1/24.
+ */
+public class MyRealm extends AuthorizingRealm {
+
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
