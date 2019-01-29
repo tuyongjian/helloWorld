74105458b22a2046072176ff170d8b8137f18b13
tuyongjian <1475591137@qq.com>
2019-01-29 18:10:38 +0800

    spring 学习。。。。。。。。。。。。

--- service/src/main/java/com/tu/service/action/ShiroAction.java
+++ service/src/main/java/com/tu/service/action/ShiroAction.java
@@ -7,6 +7,8 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.util.SavedRequest;
+import org.apache.shiro.web.util.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by len on 2019/1/24.
@@ -53,7 +57,8 @@ public class ShiroAction {
     //登录认证
      @RequestMapping(value = "/shiro-login",method = RequestMethod.GET)
      public String login1(@RequestParam("username") String username,
-                         @RequestParam("password") String password){
+                         @RequestParam("password") String password,
+                          HttpServletRequest request){
          Subject subject = SecurityUtils.getSubject();
          UsernamePasswordToken token = new UsernamePasswordToken(username,password);
          try {
@@ -63,6 +68,17 @@ public class ShiroAction {
              System.out.println("登陆失败: " + ae.getMessage());
              return "/shiro/login";
          }
-         return "/shiro/success";
+         SavedRequest savedRequest = WebUtils.getSavedRequest(request);
+         logger.info("-------------"+request.getScheme());
+         logger.info("-------------"+request.getServerName());
+         logger.info("-------------"+request.getServerPort());
+         logger.info("-------------"+savedRequest.getRequestUrl());
+         logger.info("-------------"+request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+savedRequest.getRequestUrl());
+         if(savedRequest!=null){
+
+             return "redirect:"+request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+savedRequest.getRequestUrl();
+         }else{
+             return "/shiro/success";
+         }
      }
 }
