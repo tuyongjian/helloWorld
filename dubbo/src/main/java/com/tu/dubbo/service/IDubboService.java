1fa376784d49b3223fff38aee3a3c56b082ebd02
HQ01UG982 <hq01ug982@mb.com>
2019-04-12 10:34:06 +0800

    。。。。。。。。。。。。。。。

---
+++ dubbo/src/main/java/com/tu/dubbo/service/IDubboService.java
@@ -0,0 +1,10 @@
+package com.tu.dubbo.service;
+
+/**
+ * @Description
+ * @Classname IDubboService
+ * @Date 2019/4/12 10:06
+ * @Created by tuyongjian
+ */
+public interface IDubboService {
+}
---
+++ dubbo/src/main/java/com/tu/dubbo/serviceImpl/DubboServiceImpl.java
@@ -0,0 +1,12 @@
+package com.tu.dubbo.serviceImpl;
+
+import com.tu.dubbo.service.IDubboService;
+
+/**
+ * @Description
+ * @Classname DubboServiceImpl
+ * @Date 2019/4/12 10:14
+ * @Created by tuyongjian
+ */
+public class DubboServiceImpl implements IDubboService {
+}
